4b2333e0fc956fbcd4f4accb82823ecbfd9df29d
txy <2434877135@qq.com>
2024-11-24 20:33:50 +0800

    课程列表接口

---
+++ mszlu-xt-web/mszlu-xt-web-service/src/main/java/com/mszlu/xt/web/service/SubjectService.java
@@ -0,0 +1,11 @@
+package com.mszlu.xt.web.service;
+
+import com.mszlu.xt.common.model.CallResult;
+
+public interface SubjectService {
+    /**
+     * 查询所有科目去重
+     * @return
+     */
+    CallResult listSubject();
+}
